37c8f482c1045eabaf8f7fbbb2ec641f0c800988
Jochen <jochen.handels@web.de>
2019-01-06 13:02:12 +0100

    Initial commit

---
+++ LoggingDemo/Utils/ILoggingBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace LoggingDemo.Utils
+{
+    public static class ILoggingBuilderExtensions
+    {
+        public static ILoggingBuilder AddXunit(this ILoggingBuilder builder, ITestOutputHelper testOutputHelper)
+        {
+            builder.AddProvider(new XunitLoggerProvider(testOutputHelper));
+            return builder;
+        }
+
+        public static ILoggingBuilder SetupDemoLogging(this ILoggingBuilder builder, ITestOutputHelper testOutputHelper)
+        {
+            builder.AddXunit(testOutputHelper);
+            builder.SetMinimumLevel(LogLevel.Information);
+            return builder;
+        }
+    }
+}
