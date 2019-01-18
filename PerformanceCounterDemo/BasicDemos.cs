c9705900ad7e393521e9db74ab23672046a93d0d
Jochen <jochen.handels@web.de>
2019-01-18 08:48:23 +0100

    Added demo for performance counters

--- PerformanceCounterDemo/BasicDemos.cs
+++ PerformanceCounterDemo/BasicDemos.cs
@@ -1,28 +1,222 @@
+using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DiagnosticsDemo.PerformanceCounterDemo
 {
     public class BasicDemos
     {
-        private const string TestCategory = "DiagnosticsDemo";
-        private const string NumberOfItemsName = "DiagnosticsDemo1";
+        private const double DefaultDuration = 3;
+        
+        // Average: measure a value over time and display the average of
+        // the last two measurements.Associated with each average counte
+        // is a base counter that tracks the number of samples involved.
+
+
+        // Difference: subtract the last measurement from the previous one and
+        // display the difference, if it is positive; if negative, they display a zero.
+
+        // Instantaneous: display the most recent measurement.
+
+        // Percentage: display calculated values as a percentage.
+        
+        // Rate: sample an increasing count of events over time and divide the change
+        // in count values by the change in time to display a rate of activity.
+
+
+        private const string TestCategoryName = "DiagnosticsDemo";
+        private const string NumberOfItemsName = "NumberOfItems64";
+        private const string CounterDeltaName = "CounterDelta64";
+        private const string CounterTimerName = "CounterTimer";
+        private const string CounterTimerInverseName = "CounterTimerInverse";
+        private const string CountPerTimeIntervalName = "CountPerTimeInterval64";
+        private const string RateOfCountsPerSecondName = "RateOfCountsPerSecond64";
+        private const string ElapsedTimeName = "ElapsedTime";
+        private const string RawFractionName = "RawFraction";
+        private const string RawFractionBaseName = "RawFractionBase";
+        private const string SampleFractionName = "SampleFraction";
+        private const string SampleFractionBaseName = "SampleFractionBase";
+        private const string AverageCountName = "AverageCount64";
+        private const string AverageCountBaseName = "AverageCount64Base";
+        private const string AverageTimerName = "AverageTimer32";
+        private const string AverageTimerBaseName = "AverageTimer32Base";
+
+        private static readonly Random RandomNumbers = new Random(0);
 
         public BasicDemos()
         {
-            if (!PerformanceCounterCategory.Exists(TestCategory))
+            if (!PerformanceCounterCategory.Exists(TestCategoryName))
             {
-                Debug.WriteLine($"The category {TestCategory} does not exist");
+                Debug.WriteLine($"The category {TestCategoryName} does not exist");
             }
         }
 
-        [Fact(DisplayName = "NumberOfItems Demo")]
+        [Fact(DisplayName = NumberOfItemsName + " Demo")]
         public void NumberOfItemsDemo()
         {
-            using (var counter = new PerformanceCounter(TestCategory, NumberOfItemsName, ""))
+            using (var counter = new PerformanceCounter(TestCategoryName, NumberOfItemsName, ""))
             {
                 counter.ReadOnly = false;
+                counter.Increment();
+            }
+        }
+
+        [Fact(DisplayName = CounterDeltaName + " Demo")]
+        public async Task CounterDeltaDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, CounterDeltaName, ""))
+            {
+                counter.ReadOnly = false;
+                await DoWork(() => counter.Increment());
+            }
+        }
+
+        [Fact(DisplayName = CounterTimerName + " Demo")]
+        public async Task CounterTimerDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, CounterTimerName, ""))
+            {
+                counter.ReadOnly = false;
+                var stopwatch = Stopwatch.StartNew();
+                try
+                {
+                    await DoWork();
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                    counter.IncrementBy(stopwatch.ElapsedTicks);
+                }
+            }
+        }
+
+        [Fact(DisplayName = CounterTimerInverseName + " Demo")]
+        public async Task CounterTimerInverseDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, CounterTimerInverseName, ""))
+            {
+                counter.ReadOnly = false;
+                var stopwatch = Stopwatch.StartNew();
+                try
+                {
+                    await DoWork();
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                    counter.IncrementBy(stopwatch.ElapsedTicks);
+                }
+            }
+        }
+
+        [Fact(DisplayName = CountPerTimeIntervalName + " Demo")]
+        public async Task CountPerTimeIntervalDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, CountPerTimeIntervalName, ""))
+            {
+                counter.ReadOnly = false;
+                await DoWork(() => counter.Increment());
+            }
+        }
+
+        [Fact(DisplayName = RateOfCountsPerSecondName + " Demo")]
+        public async Task RateOfCountsPerSecondDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, RateOfCountsPerSecondName, ""))
+            {
+                counter.ReadOnly = false;
+                await DoWork(() => counter.Increment());
+            }
+        }
+
+        [Fact(DisplayName = RawFractionName + " Demo")]
+        public void RawFractionDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, RawFractionName, ""))
+            using (var baseCounter = new PerformanceCounter(TestCategoryName, RawFractionBaseName, ""))
+            {
+                counter.ReadOnly = false;
+                baseCounter.ReadOnly = false;
                 counter.IncrementBy(1);
+                baseCounter.IncrementBy(1);
+            }
+        }
+
+        [Fact(DisplayName = SampleFractionName + " Demo")]
+        public void SampleFractionDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, SampleFractionName, ""))
+            using (var baseCounter = new PerformanceCounter(TestCategoryName, SampleFractionBaseName, ""))
+            {
+                counter.ReadOnly = false;
+                baseCounter.ReadOnly = false;
+                if (RandomNumbers.Next()%3 == 0)
+                {
+                    counter.IncrementBy(1);
+                }
+                baseCounter.IncrementBy(1);
+            }
+        }
+
+        [Fact(DisplayName = AverageCountName + " Demo")]
+        public void AverageCountDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, AverageCountName, ""))
+            using (var baseCounter = new PerformanceCounter(TestCategoryName, AverageCountBaseName, ""))
+            {
+                counter.ReadOnly = false;
+                baseCounter.ReadOnly = false;
+                if (RandomNumbers.Next() % 3 == 0)
+                {
+                    counter.IncrementBy(1);
+                }
+                baseCounter.IncrementBy(1);
+            }
+        }
+
+        [Fact(DisplayName = AverageTimerName + " Demo")]
+        public async Task AverageTimerDemo()
+        {
+            using (var counter = new PerformanceCounter(TestCategoryName, AverageTimerName, ""))
+            using (var baseCounter = new PerformanceCounter(TestCategoryName, AverageTimerBaseName, ""))
+            {
+                counter.ReadOnly = false;
+                baseCounter.ReadOnly = false;
+                var stopwatch = Stopwatch.StartNew();
+                try
+                {
+                    await DoWork();
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                    counter.IncrementBy(stopwatch.ElapsedTicks);
+                    baseCounter.IncrementBy(1);
+                }
+            }
+        }
+
+        private static async Task DoWork(double seconds = DefaultDuration)
+        {
+            var duration = TimeSpan.FromSeconds(seconds);
+            await Task.Delay(duration);
+        }
+
+        private static async Task DoWork(Action action, double seconds = DefaultDuration)
+        {
+            var maxTotalDuration = TimeSpan.FromSeconds(seconds);
+            var actualTotalDuration = TimeSpan.Zero;
+            while (actualTotalDuration >= maxTotalDuration)
+            {
+                action();
+                var delayDuration = TimeSpan.FromMilliseconds(RandomNumbers.Next(200, 500));
+                if (actualTotalDuration >= maxTotalDuration)
+                {
+                    delayDuration = maxTotalDuration - actualTotalDuration;
+                };
+                await Task.Delay(delayDuration);
+                actualTotalDuration += delayDuration;
             }
         }
     }
--- Scripts/setup-counter.ps1
+++ Scripts/setup-counter.ps1
@@ -3,6 +3,7 @@
         Demo for creating a performance counter
     .LINKS
         https://www.msxfaq.de/code/powershell/psperfcounter.htm#performancecounter_anlegen
+        https://manski.net/2013/11/windows-performance-counter-types/
         https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.performancecountercategory.create
         https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.performancecountertype
     .DESCRIPTION
@@ -11,7 +12,6 @@
 
 $categoryName = "DiagnosticsDemo"
 $categoryHelp = "Demo for performance counters"
-$counterName = "DiagnosticsDemo1"
 
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
     Write-Error "You must have elevated privileges on the computer to create a new performance counter."
@@ -24,16 +24,38 @@ If ([System.Diagnostics.PerformanceCounterCategory]::Exists($categoryName)) {
     Exit 0
 }
 
+$types = @(
+    @{ Main = "NumberOfItems64" },
+    @{ Main = "CounterDelta64" },
+    @{ Main = "CounterTimer" },
+    @{ Main = "CounterTimerInverse" },
+    @{ Main = "CountPerTimeInterval64" },
+    @{ Main = "RateOfCountsPerSecond64" },
+    @{ Main = "ElapsedTime" },
+    @{ Main = "RawFraction"; Base = "RawBase" },
+    @{ Main = "SampleFraction"; Base = "SampleBase" },
+    @{ Main = "AverageCount64"; Base = "AverageBase" },
+    @{ Main = "AverageTimer32"; Base = "AverageBase" }
+)
+
 Write-Host "Creating category $categoryName"
-$counterData = New-Object System.Diagnostics.CounterCreationDataCollection
-$counter = New-Object System.Diagnostics.CounterCreationData
-$counter.CounterType = [System.Diagnostics.PerformanceCounterType]::NumberOfItems64
-$counter.CounterName = $counterName
-$counterData.Add($counter)
-[System.Diagnostics.PerformanceCounterCategory]::Create( `
-    $categoryName, `
-    $categoryHelp, `
-    [System.Diagnostics.PerformanceCounterCategoryType]::SingleInstance, `
-    $counterData)
+$counters = New-Object System.Diagnostics.CounterCreationDataCollection
+ForEach ($type in $types) {
+    $counter = New-Object System.Diagnostics.CounterCreationData
+    $counter.CounterType = [System.Diagnostics.PerformanceCounterType]$type.Main
+    $counter.CounterName = $type.Main
+    $counters.Add($counter) | Out-Null
+    If ($type.ContainsKey("Base")) {
+        $counter = New-Object System.Diagnostics.CounterCreationData
+        $counter.CounterType = [System.Diagnostics.PerformanceCounterType]$type.Base
+        $counter.CounterName = $type.Main + "Base"
+        $counters.Add($counter) | Out-Null
+    }
+}
+[System.Diagnostics.PerformanceCounterCategory]::Create(
+    $categoryName,
+    $categoryHelp,
+    [System.Diagnostics.PerformanceCounterCategoryType]::SingleInstance,
+    $counters)
 
 Write-Host "Exiting, execute the script a second time to use the counter. This allows time for the operating system to refresh its list of registered counters and their configuration."
